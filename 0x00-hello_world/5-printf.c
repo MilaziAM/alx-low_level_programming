162a80b89c9ae59ad15a372294363f2bb59e1073
MilaziAM <abdulmsizimilazi@outlook.com>
2023-02-16 17:07:15 -0800

    e

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
- * main - prints out string in stdout 
+/**
+ * main - prints out string in stdout
  * Return: 0 if successful
  */
 int main(void)
 {
-printf("
-with proper grammar, but the outcome is a piece of art,
-\n");
-return (0);
+	printf("
+	with proper grammar, but the outcome is a piece of art,
+	\n");
+
+	return (0);
 }
