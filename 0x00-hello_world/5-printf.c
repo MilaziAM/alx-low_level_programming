415cae4e0c8cd5d6c8489b363b3fd10001fc0e43
MilaziAM <abdulmsizimilazi@outlook.com>
2023-02-16 16:56:24 -0800

    commit 9

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-/*main- prints out string in stdout and return value to 0 if successful*/
+/*
+ * main - prints out string in stdout 
+ * Return: 0 if successful
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("
+with proper grammar, but the outcome is a piece of art,
+\n");
+
 	return (0);
 }
