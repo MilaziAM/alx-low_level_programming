d799a9fbe5fa6afbbb3ce7a8f38f192bbc79e217
MilaziAM <abdulmsizimilazi@outlook.com>
2023-02-16 16:46:48 -0800

    commit 8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - print out as string in stdout 
  * Return: 0 if successful
@@ -8,8 +7,7 @@
 
 int main(void)
 {
-	puts("\"
-			Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
