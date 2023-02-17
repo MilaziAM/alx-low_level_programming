de978b40215d9afbdaac81c13d72750705962edc
MilaziAM <abdulmsizimilazi@outlook.com>
2023-02-16 16:23:33 -0800

    c-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <studio.h>
+/* main print out as string in stdout and return value to 0 if successful
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
