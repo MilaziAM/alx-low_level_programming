ba8a4f19af6c9e1a8dc37fd7dfb6360b354cf48d
MilaziAM <abdulmsizimilazi@outlook.com>
2023-02-16 16:34:44 -0800

    commit 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-/* main print out as string in stdout and return value to 0 if successful
- */
+/*main- print out as string in stdout and return value to 0 if successful*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-/*main prints out string in stdout and return value to 0 if successful
-*/
+/*main- prints out string in stdout and return value to 0 if successful*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
