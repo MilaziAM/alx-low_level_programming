80faffabf8dbc0bb3e962a10b747c6b1e9320880
MilaziAM <abdulmsizimilazi@outlook.com>
2023-02-21 13:07:06 -0800

    '

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - betty style doc
+ * betty style coding
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+	char *h = "_putchar\n";
+	while (*h)
+	{
+		_putchar(*h);
+		h++;
+	}
+	return (0);
+}
